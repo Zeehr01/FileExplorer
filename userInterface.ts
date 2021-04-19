111c1aa8d642686a27de09973c048555b246ffa1
Zahra <biadyzahra12@gmail.com>
2021-04-19 11:17:14 +0100

    last update

---
+++ userInterface.ts
@@ -0,0 +1,95 @@
+'use strict';
+let document;
+const fileSystem = require('./fileSystem');
+const path = require('path');
+
+
+function displayFolderPath(folderPath) {
+    document.getElementById('current-folder')
+    .innerHTML = convertFolderPathIntoLinks(folderPath);
+    bindCurrentFolderPath();
+   }
+
+function clearView() {
+ const mainArea = document.getElementById('main-area');
+ let firstChild = mainArea.firstChild;
+ while (firstChild) {
+ mainArea.removeChild(firstChild);
+ firstChild = mainArea.firstChild;
+ }
+}
+function loadDirectory(folderPath) {
+ return function (window) {
+ if (!document) document = window.document;
+ displayFolderPath(folderPath);
+ fileSystem.getFilesInFolder(folderPath, (err, files) => {
+ clearView();
+ if (err) {
+ return alert('Sorry, you could not load your folder');
+ }
+ fileSystem.inspectAndDescribeFiles(folderPath, files, displayFiles);
+ });
+ };
+}
+
+function displayFile(file) {
+    const mainArea = document.getElementById('main-area');
+    const template = document.querySelector('#item-template');
+    let clone = document.importNode(template.content, true);
+    
+    clone.querySelector('img').src = `images/${file.type}.svg`;
+    clone.querySelector('img').setAttribute('data-filePath', file.path);
+    if (file.type === 'directory') {
+    clone.querySelector('img')
+    .addEventListener('dblclick', () => {
+    loadDirectory(file.path)();
+    }, false);
+    } else {
+    clone.querySelector('img')
+    .addEventListener('dblclick', () => {
+    fileSystem.openFile(file.path);
+    }, false);
+    }
+    clone.querySelector('.filename').innerText = file.file;
+    mainArea.appendChild(clone);
+   }
+
+   function displayFiles(err, files) {
+    if (err) {
+    return alert('Sorry, you could not display your files');
+    }
+    files.forEach(displayFile);
+   }
+
+   function bindDocument (window) {
+    if (!document) {
+    document = window.document;
+    }
+   }
+
+   function convertFolderPathIntoLinks (folderPath) {
+    const folders = folderPath.split(path.sep);
+    const contents = [];
+    let pathAtFolder = '';
+    folders.forEach((folder) => {
+    pathAtFolder += folder + path.sep;
+    contents.push(`<span class="path" data-path="${pathAtFolder.slice(0,
+        -1)}">${folder}</span>`);
+         });
+         return contents.join(path.sep).toString();
+        }
+
+
+        function bindCurrentFolderPath() {
+            const load = (event) => {
+            const folderPath = event.target.getAttribute('data-path');
+            loadDirectory(folderPath)();
+            };
+            const paths = document.getElementsByClassName('path');
+            for (var i = 0; i < paths.length; i++) {
+            paths[i].addEventListener('click', load, false);
+            }
+           }
+
+
+   module.exports = { bindDocument, displayFiles, loadDirectory,bindCurrentFolderPath}; 
